a30b1b5ac92cedb678033492a4873eed6e245917
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2025-04-30 16:41:16 +1000

    Add Chrome driver options such as headless mode to run using GitHub Actions.

--- src/test/java/jp/funk/utils/WebDriverManager.java
+++ src/test/java/jp/funk/utils/WebDriverManager.java
@@ -2,13 +2,27 @@ package jp.funk.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class WebDriverManager {
     private static WebDriver driver;
 
     public static void setUp() {
+        ChromeOptions options = new ChromeOptions();
+
         if (driver == null) {
-            driver = new ChromeDriver();
+            try {
+                Path tmpDir = Files.createTempDirectory("chrome-user-data");
+                options.addArguments("--user-data-dir=" + tmpDir.toAbsolutePath());
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to create temporary user data directory", e);
+            }
+
+            options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+            driver = new ChromeDriver(options);
             driver.manage().window().maximize();
         }
     }
